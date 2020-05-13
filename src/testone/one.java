4540a0b3654d11a61525a85ac9faea705eb1be82
yu3739 <373936215@qq.com>
2020-05-13 18:26:23 +0800

    Initial commit

---
+++ src/testone/one.java
@@ -0,0 +1,83 @@
+package testone;
+
+/**
+ * Created by YuZhenni on2020/5/12.
+ */
+public class one {
+    public static void main(String[] args) {
+       //这是一个注释
+
+      /*  int avg1=48;
+        double rise=5.43;
+        double avg2=avg1+rise;
+        System.out.println(avg1);
+        System.out.println(avg2);
+
+        final String People="一个人";
+
+        System.out.println(People);
+
+
+        int age1=1;
+        int age2=2;
+        int age3=3;
+        int age4=4;
+
+        int age5=age1+age2;
+        System.out.println(age5);
+        age5+=age1;
+        System.out.println(age5);
+        age5-=age2;
+        System.out.println(age5);
+
+
+       int a=33;
+       double b=3.22;
+       String a1="hee";
+       String b1="ddd";
+        System.out.println("a等于b?"+(a==b));
+        System.out.println("a大于b?"+(a1=b1));
+        System.out.println("a小于等于b?"+(a<=b));
+        System.out.println("a1等于b1?"+(a1==b1));
+
+
+        boolean a=true;
+        boolean b=false;
+        boolean c=false;
+        boolean d=true;
+
+        System.out.println("未通过"+(a&&b));
+        System.out.println("通过"+(a||b));
+        System.out.println("通过"+(c^d));
+        System.out.println("通过"+(!c));
+
+        int score = 44;
+       // String a=(score>44)? "对":"不对";
+
+        if (score%4==6) {
+            System.out.println(score);
+        }
+        else if (score*1==44){
+        System.out.println("ddddd");
+    }
+        else {
+            System.out.println("不对啊");
+        }
+    }*/
+
+
+      int age=22;
+        if (age >60) {
+            System.out.println("老年");
+        }
+        else if (age>40){
+            System.out.println("中年");
+        }
+        else if (age>18){
+            System.out.println("少年");
+        }
+        else{
+            System.out.println("童年");
+        }
+    }
+}
