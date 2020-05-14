d8b1a869711277e92542486830361a72598f227d
yu3739 <373936215@qq.com>
2020-05-14 18:47:08 +0800

    第三次测试提交更新到git

--- src/testone/one.java
+++ src/testone/one.java
@@ -5,7 +5,7 @@ package testone;
  */
 public class one {
     public static void main(String[] args) {
-       //这是一个注释
+        //这是一个注释
 
       /*  int avg1=48;
         double rise=5.43;
@@ -78,7 +78,7 @@ public class one {
             System.out.println("童年");
         }*/
 
-      int score =99;
+    /*  int score =99;
       String sex="女";
         if (score>=90) {
             if(sex.equals("女")) {
@@ -90,9 +90,166 @@ public class one {
         }
         else{
             System.out.println("分数太低");
-        }
+        }*/
 
 
+
+        /*char today='一';
+        switch(today){
+            case '一':
+            case '三':
+            case '五':
+                System.out.println("包子");
+                //break;
+            case '二':
+            case '四':
+            case '六':
+                System.out.println("油条");
+                break;
+            case '日':
+                System.out.println("套餐");
+
+        }*/
+
+
+/*
+
+            int i=1;
+            while (i<=5){
+                i++;
+                System.out.println(i);
+
+            }
+*/
+       /*
+            int num=2;
+            int sum=0;
+            do {
+
+            sum=sum+num;
+            num=num+2;
+            System.out.println("偶数只和："+sum);
+           }
+           while (num<=50);*/
+
+         /*int sum=0;//保存不能被3整除的数之和
+       // 循环变量 i 初始值为 1 ,每执行一次对变量加 1，只要小于等于 10 就重复执行循环
+        for (int i = 1; i <=10; i++) {
+            // 变量 i 与 3 进行求模（取余），如果不等于 0 ，则表示不能被 3 整除
+            if(i%3!=0) {
+                sum = sum + i;
+
+            }
+            System.out.println(sum);
+        }*/
+/*
+        int i = 1;
+        for (; i <=10 ; i++) {
+            if((i>2)&&(i%3==0)){
+
+                break;
+            }
+
+            System.out.println("结束");
+
         }
+
+        System.out.println(i);*/
+
+         /*int sum=0;
+        for (int i = 1; i <=10 ; i++) {
+            //每次循环累加求和
+            sum=sum+i;
+            //判断累加值是否大于20，大于20就打印出sum的值并且推出循环
+            if(sum>20){
+                System.out.println(sum);
+                //退出循环
+                break;
+            }
+        }*/
+
+
+
+        /* int sum=0;
+        int i = 1;
+        for (; i <=10 ; i++) {
+
+            if(i%2!=0){
+                continue;
+            }
+            sum=sum+i;
+            System.out.println(i);
+
+        }
+
+        System.out.println(sum);*/
+
+     /*  //外层循环控制打印行数
+        for (int i = 1; i <3 ; i++) {
+            //内层循环控制每行打印的*号数
+            for (int j = 1; j <4 ; j++) {
+                System.out.print("*");
+            }
+            //每行打印完成换行
+          System.out.println();
+        }*/
+
+
+        /*for (int i = 1; i <=3 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+
+       /*    int a =0, b=0;
+       do{
+           System.out.println();
+           a++;
+           b=0;
+           //b的值比a小时就输出一个*号
+          do{
+            System.out.print("*");
+            b++;
+             }while(b<a);
+
+        }while(a<=5);*/
+
+
+       /*//把控制台输入的值获取存到“shuru"里
+        Scanner shuru=new Scanner(System.in);
+
+        System.out.println("输入");
+        //把“shuru"里的值赋值给num
+        int num=shuru.nextInt();
+        int changdu=0;
+
+
+        if(num>=0){
+            while(num!=0){
+                //当数字不为0时，长度+1
+                changdu++;
+                //把数字除以10用于缩减一位
+                num/=10;
+            }
+            System.out.println(changdu);
+        }
+*/
+
+
+        int chengji = 53;
+        int cishu = 0;
+        System.out.println("加分前成绩"+chengji);
+       while(chengji <60) {
+           chengji++;
+         cishu++;
+        }
+        System.out.println("加分后成绩"+chengji);
+        System.out.println(cishu);
+
     }
+
+    }
+
 
